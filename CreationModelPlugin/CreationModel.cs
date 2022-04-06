8a18f6e34e8c81ab90692e4ec8ac0260026eaa98
Евгения Волкова <genya.volkova@gmail.com>
2022-04-07 00:50:03 +0300

    Изменение 2

--- CreationModelPlugin/CreationModel.cs
+++ CreationModelPlugin/CreationModel.cs
@@ -1,4 +1,5 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
@@ -77,7 +78,92 @@ namespace CreationModelPlugin
                 AddWindow(doc, level1, walls[i]);
             }
 
+            //AddRoof(doc, level2, walls);
+            AddExtrusionRoof(doc, level2, walls, width, depth);
+
             transaction1.Commit();
+        }
+
+        private void AddExtrusionRoof(Document doc, Level level2, List<Wall> walls, double width, double depth)
+        {
+            RoofType roofType = new FilteredElementCollector(doc)
+                 .OfClass(typeof(RoofType))
+                 .OfType<RoofType>()
+                 .Where(x => x.Name.Equals("Типовой - 400мм"))
+                 .Where(x => x.FamilyName.Equals("Базовая крыша"))
+                 .FirstOrDefault();
+
+            View view = new FilteredElementCollector(doc)
+                 .OfClass(typeof(View))
+                 .OfType<View>()
+                 .Where(x => x.Name.Equals("Уровень 1"))
+                 .FirstOrDefault();
+
+            double wallWidth = walls[0].Width;
+            double dt = wallWidth / 2;
+
+            double extrusionStart = -width / 2 - dt;
+            double extrusionEnd = width / 2 + dt;
+
+            double curveStart = -depth / 2 - dt;
+            double curveEnd = +depth / 2 + dt;
+
+            CurveArray curveArray = new CurveArray();
+            curveArray.Append(Line.CreateBound(new XYZ(0, curveStart, level2.Elevation), new XYZ(0, 0, level2.Elevation + 10)));
+            curveArray.Append(Line.CreateBound(new XYZ(0,0,level2.Elevation+10), new XYZ(0, curveEnd, level2.Elevation)));
+
+            ReferencePlane plane = doc.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 20), new XYZ(0, 20, 0), view);
+            ExtrusionRoof extrusionRoof = doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, extrusionStart, extrusionEnd);
+            extrusionRoof.EaveCuts = EaveCutterType.TwoCutSquare;
+        }
+
+        private void AddRoof(Document doc, Level level2, List<Wall> walls)
+        {
+            RoofType roofType = new FilteredElementCollector(doc)
+                 .OfClass(typeof(RoofType))
+                 .OfType<RoofType>()
+                 .Where(x => x.Name.Equals("Типовой - 400мм"))
+                 .Where(x => x.FamilyName.Equals("Базовая крыша"))
+                 .FirstOrDefault();
+
+            double wallWidth = walls[0].Width;
+            double dt = wallWidth / 2;
+            List<XYZ> points = new List<XYZ>(); 
+            points.Add(new XYZ(-dt, -dt, 0));
+            points.Add(new XYZ(dt, -dt, 0));
+            points.Add(new XYZ(dt, dt, 0));
+            points.Add(new XYZ(-dt, dt, 0));
+            points.Add(new XYZ(-dt, -dt, 0));
+
+            Application application = doc.Application;
+            CurveArray footprint = application.Create.NewCurveArray();
+
+            for (int i = 0; i < 4; i++)
+            {
+                LocationCurve curve = walls[i].Location as LocationCurve;
+                XYZ p1 = curve.Curve.GetEndPoint(0);
+                XYZ p2 = curve.Curve.GetEndPoint(1);
+                Line line = Line.CreateBound(p1+points[i], p2+points[i+1]);
+                footprint.Append(line);
+
+            }
+            ModelCurveArray footPrintToModelCurveMapping = new ModelCurveArray();
+            FootPrintRoof footPrintRoof = doc.Create.NewFootPrintRoof(footprint, level2, roofType, out footPrintToModelCurveMapping);
+            //ModelCurveArrayIterator iterator = footPrintToModelCurveMapping.ForwardIterator();
+            //iterator.Reset();
+            //while(iterator.MoveNext())
+            //{
+            //    ModelCurve modelCurve = iterator.Current as ModelCurve;
+            //    footPrintRoof.set_DefinesSlope(modelCurve, true);
+            //    footPrintRoof.set_SlopeAngle(modelCurve, 0.5);
+            //}
+
+            foreach(ModelCurve m in footPrintToModelCurveMapping)
+            {
+                footPrintRoof.set_DefinesSlope(m, true);
+                footPrintRoof.set_SlopeAngle(m, 0.5);
+            }
+
         }
 
         private XYZ GetElementCenter(Element element)
